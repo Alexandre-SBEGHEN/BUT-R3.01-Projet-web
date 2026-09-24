837f8f1b3cc30f3a6518bad1643de8f918edfffe
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-09-24 17:08:01 +0200

    fix(routeur): Utilisation des classes des pages et correction nom fichier

--- website/assets/utils/utils.inc.php
+++ website/assets/utils/utils.inc.php
@@ -1,19 +1,25 @@
 <?php
 
-function page_start(string $title, string $description, string $css_path = ''): void {?>
+function page_start(string $title, string $description, string $css_path = ''): void {
+    $css_element = ($css_path !== '') ? "\n\t<link rel='stylesheet' type='text/css' href='$css_path'>" : '';
+
+    echo <<< HTML
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title><?php echo $title; ?></title>
+    <title>$title</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $description; ?>">
-    <?php if (!empty($css_path)) { echo '<link rel="stylesheet" href="' . $css_path . '" >'; }  ?>
+    <meta name="description" content="$description">$css_element
 </head>
-<body><?php
+<body>
+
+HTML;
 }
 
-function page_end(): void {?>
+function page_end(): void {
+    echo <<< HTML
 </body>
-</html><?php
+</html>
+HTML;
 }
