f5e5435dbbbb2c2b929b21fc636ec27ad5f70351
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-09-25 22:21:04 +0200

    fix(home_view): Oubli suppression du require

--- website/modules/views/home_view.php
+++ website/modules/views/home_view.php
@@ -2,15 +2,13 @@
 
 namespace modules\views;
 
-require 'assets/includes/utils.inc.php';
-
 class home_view {
     public function show(): void {
         template_view::html_begin('Cyber Cigales', 'Un Escape Game numérique pour apprendre à protéger tes données');
         template_view::page_header();
         echo <<< HTML
     <main>
-        <h1>Cyber Cigales</h1>
+        <p>main</p>
     </main>
 
 HTML;
